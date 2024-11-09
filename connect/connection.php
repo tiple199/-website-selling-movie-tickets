cc62695cf4981012ab33cf3eba1602fc0b9f7927
Da Nhat Nguyen <nguyendanhat2134@gmail.com>
2024-11-09 12:08:14 +0700

    new update

--- connect/connection.php
+++ connect/connection.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost:3307","root","","doan");
+    $conn = new mysqli("localhost","root","","doan");
     if($conn->connect_error){
         die("Connection failed: " .$conn->connect_error);
     }
